e1f1ed310e1a5ffbf441d7651ebb4ee9a60982eb
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2015-08-22 21:35:00 -0400

    Initial commit.

---
+++ main.js
@@ -0,0 +1,20 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+require('crash-reporter').start();
+var mainWindow = null;
+
+app.on('ready', function(){
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 800
+  });
+
+  mainWindow.loadUrl("file://" + __dirname + "/index.html");
+
+  //mainWindow.openDevTools();
+
+  mainWindow.on('closed', function(){
+    mainWindow = null;
+  });
+
+});
